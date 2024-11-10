4668bc7d0494b6f8fba88e05cc99e4a750a08f4a
chyves <82107846+notchyves@users.noreply.github.com>
2024-11-10 16:10:10 -0500

    better

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,15 +1,24 @@
 #include <Windows.h>
 #include "src/Selaura.h"
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lp) {
-    if (GetModuleHandleA("Minecraft.Windows.exe") != GetModuleHandleA(NULL)) {
-    	// app injected is NOT Minecraft
-    	return TRUE;
+DWORD WINAPI StartRoutine(HINSTANCE hinstDLL) {
+    if (GetModuleHandleA("Minecraft.Windows.exe") == NULL) {
+        return 0;
     }
 
+    Selaura::get().init(hinstDLL);
+    return 0;
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lp) {
     if (fdwReason == DLL_PROCESS_ATTACH) {
-    	Selaura::get().init(hinstDLL);
+        DisableThreadLibraryCalls(hinstDLL);
+        HANDLE hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)StartRoutine, hinstDLL, 0, NULL);
+        if (hThread) {
+            CloseHandle(hThread);
+        }
+        Selaura::get().~Selaura();
     }
-	
+
     return TRUE;
 }
