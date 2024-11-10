72e9a6e71a9bb4a96f6c1db1ca367381987efca3
chyves <82107846+notchyves@users.noreply.github.com>
2024-11-10 15:58:48 -0500

    cpp

---
+++ src/Selaura.cpp
@@ -0,0 +1,14 @@
+#include "Selaura.h"
+
+namespace {
+	// buffers for saving
+	alignas(Selaura) char SelauraBuffer[sizeof(Selaura)] = {};
+};
+
+void Selaura::init(HINSTANCE hInst) {
+	new (SelauraBuffer) Selaura;
+}
+
+Selaura& Selaura::get() noexcept {
+    return *std::launder(reinterpret_cast<Selaura*>(SelauraBuffer));
+}
