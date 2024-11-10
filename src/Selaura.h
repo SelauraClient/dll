323b0588e61532f10bd6fa2b710c5c35960715e6
chyves <82107846+notchyves@users.noreply.github.com>
2024-11-10 16:04:17 -0500

    fix error part 1

--- src/Selaura.h
+++ src/Selaura.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <new>
+#include <Windows.h> // Add this line to include HINSTANCE
 
 class Selaura {
 public:
-	// gets
-	static Selaura& get();
+    // gets
+    static Selaura& get() noexcept;
 
-	//funcs
-	void init(HINSTANCE hInst);
+    // funcs
+    void init(HINSTANCE hInst);
 };
