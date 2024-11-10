5bb1ab9f833f1ff9ab98cd291a0e8c26242f4e91
chyves <82107846+notchyves@users.noreply.github.com>
2024-11-10 16:19:29 -0500

    window title!

--- src/Selaura.cpp
+++ src/Selaura.cpp
@@ -1,5 +1,8 @@
 #include "Selaura.h"
 
+using namespace winrt::Windows::UI::ViewManagement;
+using namespace winrt::Windows::ApplicationModel::Core;
+
 namespace {
     // buffers for saving
     alignas(Selaura) char SelauraBuffer[sizeof(Selaura)] = {};
@@ -7,6 +10,23 @@ namespace {
 
 void Selaura::init(HINSTANCE hInst) {
     new (SelauraBuffer) Selaura;
+
+    const winrt::Windows::ApplicationModel::Package package = winrt::Windows::ApplicationModel::Package::Current();
+    auto [major, minor, build, revision] = package.Id().Version();
+    std::wstring version = std::to_wstring(major) + L"." + std::to_wstring(minor) + L"." + std::to_wstring(build) + L"." + std::to_wstring(revision);
+
+    { // window title
+        std::wstring build_str = std::to_wstring(build);
+        if (build_str.length() > 2) {
+            build_str = build_str.substr(0, build_str.length() - 2);
+        }
+
+        std::wstring title_version = std::to_wstring(major) + L"." + std::to_wstring(minor) + L"." + build_str;
+        std::wstring title = L"Selaura Client v" + title_version;
+        CoreApplication::MainView().CoreWindow().DispatcherQueue().TryEnqueue([title = std::move(title)]() {
+            ApplicationView::GetForCurrentView().Title(title);
+        });
+    }
 }
 
 Selaura& Selaura::get() noexcept {
