5f7614f0e4fef5656dd9ced342ddd6fb9f1ecfe3
chyves <82107846+notchyves@users.noreply.github.com>
2024-11-23 20:52:36 +0000

    WHYY

--- src/log/LogManager.h
+++ src/log/LogManager.h
@@ -7,12 +7,24 @@
 #include <sstream>
 #include <mutex>
 #include <Windows.h>
+#include <filesystem>
+#include <fstream>
 
 class LogManager {
 public:
-    LogManager() = default;
+    LogManager() {
+        init();
+    }
 
     void init() {
+        // Set up the log folder
+        char path[MAX_PATH];
+        ExpandEnvironmentStringsA("%localappdata%\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\RoamingState\\Selaura\\logs", path, MAX_PATH);
+        logFolder = path;
+
+        // Create the log directory if it doesn't exist
+        std::filesystem::create_directories(logFolder);
+
         MessageBoxA(nullptr, "LogManager initialized.", "Log Info", MB_OK);
     }
 
@@ -32,19 +44,40 @@ public:
     }
 
 private:
+    std::string logFolder;
     std::mutex logMutex;
 
     template<typename... Args>
     void log(const std::string& level, const Args&... args) {
         std::lock_guard<std::mutex> lock(logMutex);
         std::string message = formatMessage(level, args...);
-        MessageBoxA(nullptr, message.c_str(), "Log Message", MB_OK);
+
+        // Write the log message to a file
+        std::ofstream logFile(getLogFilePath(), std::ios::app);
+        if (logFile.is_open()) {
+            logFile << message << std::endl;
+            logFile.close();
+        } else {
+            MessageBoxA(nullptr, "Failed to open log file.", "Log Error", MB_OK | MB_ICONERROR);
+        }
     }
 
     template<typename... Args>
     std::string formatMessage(const std::string& level, const Args&... args) const {
         std::ostringstream oss;
         oss << "[" << level << "] " << std::vformat("{}", std::make_format_args(args...));
+        return oss.str();
+    }
+
+    std::string getLogFilePath() const {
+        auto now = std::chrono::system_clock::now();
+        auto in_time_t = std::chrono::system_clock::to_time_t(now);
+        std::tm buf;
+        localtime_s(&buf, &in_time_t);
+
+        std::ostringstream oss;
+        oss << logFolder << "\\Selaura_"
+            << std::put_time(&buf, "%Y-%m-%d_%H-%M-%S") << ".txt";
         return oss.str();
     }
 };
