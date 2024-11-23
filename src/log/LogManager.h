5985744427bfb7ce93b2fe2e72cd450c44da474d
chyves <82107846+notchyves@users.noreply.github.com>
2024-11-23 20:44:39 +0000

    ?

--- src/log/LogManager.h
+++ src/log/LogManager.h
@@ -1,15 +1,11 @@
 #pragma once
 
-#include <filesystem>
-#include <fstream>
 #include <string>
 #include <chrono>
 #include <ctime>
 #include <iomanip>
 #include <sstream>
 #include <mutex>
-#include <iostream>
-#include <format>
 #include <Windows.h>
 
 class LogManager {
@@ -17,11 +13,7 @@ public:
     LogManager() = default;
 
     void init() {
-        char path[MAX_PATH];
-        ExpandEnvironmentStringsA("%localappdata%\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\RoamingState\\Selaura\\logs", path, MAX_PATH);
-        logFolder = path;
-
-        std::filesystem::create_directories(logFolder);
+        MessageBoxA(nullptr, "LogManager initialized.", "Log Info", MB_OK);
     }
 
     template<typename... Args>
@@ -40,33 +32,19 @@ public:
     }
 
 private:
-    std::string logFolder;
     std::mutex logMutex;
 
     template<typename... Args>
     void log(const std::string& level, const Args&... args) {
         std::lock_guard<std::mutex> lock(logMutex);
-        std::ofstream logFile(getLogFilePath(), std::ios::app);
-        if (logFile.is_open()) {
-            logFile << "[" << level << "] " << formatMessage(args...) << std::endl;
-        } else {
-            std::terminate();
-        }
+        std::string message = formatMessage(level, args...);
+        MessageBoxA(nullptr, message.c_str(), "Log Message", MB_OK);
     }
 
     template<typename... Args>
-    std::string formatMessage(const Args&... args) const {
-        return std::vformat("{}", std::make_format_args(args...));
-    }
-
-    std::string getLogFilePath() const {
-        auto now = std::chrono::system_clock::now();
-        auto in_time_t = std::chrono::system_clock::to_time_t(now);
-        std::tm buf;
-        localtime_s(&buf, &in_time_t);
+    std::string formatMessage(const std::string& level, const Args&... args) const {
         std::ostringstream oss;
-        oss << logFolder << "\\Selaura_"
-            << std::put_time(&buf, "%Y-%m-%d_%H-%M-%S") << ".txt";
+        oss << "[" << level << "] " << std::vformat("{}", std::make_format_args(args...));
         return oss.str();
     }
 };
